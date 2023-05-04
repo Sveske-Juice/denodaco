1980e4469585481dbbecbc710e8dc3accd54d691
Sveske-Juice <carl.benjamin.dreyer@gmail.com>
2023-05-04 22:21:56 +0200

    add user WIP

--- server/src/database.js
+++ server/src/database.js
@@ -41,8 +41,81 @@ function cleanup()
     })
 }
 
+// HELPER METHODS FOR DB HANDLING
+
+async function addUser(userData)
+{
+    let newUser = userData;
+
+    // Sanitize user inputted data
+    newUser["password"] = mysql.escape(newUser["password"]);
+    newUser["username"] = mysql.escape(newUser["username"]);
+    newUser["first_name"] = mysql.escape(newUser["first_name"]);
+    newUser["middle_names"] = mysql.escape(newUser["middle_names"]);
+    newUser["last_name"] = mysql.escape(newUser["last_name"]);
+    newUser["country_code"] = mysql.escape(newUser["country_code"]);
+    newUser["birthday"] = mysql.escape(newUser["birthday"]);
+    newUser["email"] = mysql.escape(newUser["email"]);
+
+    if (newUser["username"].length > 255)
+        return "ERROR: Username to long";
+
+    if (newUser["first_name"].length > 255)
+        return "ERROR: First name to long";
+
+    if (newUser["middle_names"].length > 255)
+        return "ERROR: Middle names to long";
+
+    if (newUser["last_name"].length > 255)
+        return "ERROR: Last name to long";
+
+    if (newUser["country_code"].length > 2)
+        return "ERROR: Country code to long";
+
+    if (newUser["email"].length > 255)
+        return "ERROR: Email to long";
+
+    connection.query(
+        `INSERT INTO users( username,
+                            first_name,
+                            middle_names,
+                            last_name,
+                            country_code,
+                            birthdate,
+                            account_creation,
+                            last_login,
+                            biography,
+                            email,
+                            is_admin,
+                            has_profile_picture,
+                            hash,
+                            salt)
+        VALUES(
+            ${newUser["username"]},
+            ${newUser["first_name"]},
+            ${newUser["middle_names"]},
+            ${newUser["last_name"]},
+            ${newUser["country_code"]},
+            ${newUser["birthdate"]},
+            ${newUser["account_creation"]},
+            ${newUser["last_login"]},
+            ${newUser["biography"]},
+            ${newUser["email"]},
+            ${newUser["is_admin"]},
+            ${newUser["has_profile_picture"]},
+            ${newUser["email"]},
+            ${newUser["hash"]},
+            ${newUser["salt"]},
+        )`
+    , (err, result) => {
+        if (err) throw err;
+        return result;
+    });
+}
+
 module.exports = 
 {
     init,
     cleanup,
+    addUser,
 }
