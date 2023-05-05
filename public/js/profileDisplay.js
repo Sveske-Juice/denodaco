060c11309e3cb6f039d2b04348e0e05bd53f745e
Sveske-Juice <carl.benjamin.dreyer@gmail.com>
2023-05-06 00:42:58 +0200

    display user info

--- public/js/profileDisplay.js
+++ public/js/profileDisplay.js
@@ -12,15 +12,15 @@ let pEmail;
 function init()
 {
     pUsername = document.querySelector("#username");
-    pUsername = document.querySelector("#first-name");
-    pUsername = document.querySelector("#middle-names");
-    pUsername = document.querySelector("#last-name");
-    pUsername = document.querySelector("#country-code");
-    pUsername = document.querySelector("#birthdate");
-    pUsername = document.querySelector("#account-creation");
-    pUsername = document.querySelector("#last-login");
-    pUsername = document.querySelector("#biography");
-    pUsername = document.querySelector("#email");
+    pFirstName = document.querySelector("#first-name");
+    pMiddleNames = document.querySelector("#middle-names");
+    pLastName = document.querySelector("#last-name");
+    pCountryCode = document.querySelector("#country-code");
+    pBirthdate = document.querySelector("#birthdate");
+    pAccountCreation = document.querySelector("#account-creation");
+    pLastLogin = document.querySelector("#last-login");
+    pBiography = document.querySelector("#biography");
+    pEmail = document.querySelector("#email");
     
     onAuthed.subscribe(displayProfileSettings);
 }
@@ -37,6 +37,17 @@ async function displayProfileSettings()
         
         // Set data
         pUsername.textContent = data["username"];
+        pFirstName.textContent = data["first_name"];
+        pMiddleNames.textContent = data["middle_names"];
+        pLastName.textContent = data["last_name"];
+        pCountryCode.textContent = data["country_code"];
+        pBirthdate.textContent = data["birthdate"];
+        pAccountCreation.textContent = data["account_creation"];
+        pLastLogin.textContent = data["last_login"];
+        pEmail.textContent = data["email"];
+
+        if (data["biography"])
+            pBiography.textContent = data["biography"];
     }
     catch (err)
     {
