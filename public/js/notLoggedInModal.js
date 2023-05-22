57eae592260c5f3274c131214ddc65cfd3d86c87
Sveske-Juice <carl.benjamin.dreyer@gmail.com>
2023-05-23 01:10:22 +0200

    not logged in modal re-stylized

--- public/js/notLoggedInModal.js
+++ public/js/notLoggedInModal.js
@@ -4,15 +4,22 @@ const template = document.createElement("template");
 
 template.innerHTML = `
 <div id="not-logged-in-modal" class="modal">
-<div class="modal-content">
-    <span id="not-logged-in-modal-close-btn" class="close">&times;</span>
-    <p>You need to be logged in to view this content.</p>
-    <center><a href="login.html">Log in</a></center>
-    <br>
-    <center><p>or</p></center>
-    <br>
-    <center><a href="signup.html">Sign up</a></center>
-</div>
+    <div class="modal-header">
+        <span id="not-logged-in-modal-close-btn" class="close">&times;</span>
+    </div>
+    <div id="not-logged-in-modal-title">
+        <h1>You need to be logged in to view this content. </h1>
+    </div>
+    <div class="modal-content">
+        <div id="not-logged-in-modal-login">
+            <p class="subtitle">Already have an account?</p>
+            <a class="primary-button" href="login.html">Login</a>
+            </div>
+            <div id="not-logged-in-modal-signup">
+            <p class="subtitle">New to Denodaco?</p>
+            <a class="primary-button" href="signup.html">Signup</a>
+        </div>
+    </div>
 </div>
 `;
 
